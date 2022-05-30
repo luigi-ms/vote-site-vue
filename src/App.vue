95b71fdea7e68396f9aaa1f86a2ceb65df2246e0
Luigi Moraes <luigimoraes3@gmail.com>
2022-05-30 20:30:35 -0300

    add: Vuetify

--- src/App.vue
+++ src/App.vue
@@ -5,41 +5,9 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+			<router-link to="/">Homepage</router-link>
+			<router-link style="color: black" to="/about">About</router-link>
     </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
   </v-app>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'font-awesome/css/font-awesome.css'
 
 Vue.config.productionTip = false
 
