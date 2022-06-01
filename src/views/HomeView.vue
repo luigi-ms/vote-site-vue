226f7c635b9d98f0eebdc968bc576741d7611771
Luigi Moraes <luigimoraes3@gmail.com>
2022-05-31 22:03:28 -0300

    HomeView using v-simple-table

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,36 @@
 <template>
-	<div>
-		<h1>Ranking</h1>
-	</div>
+	<v-row>
+		<v-col cols=12 md=8>
+			<h1>Ranking</h1>
+		</v-col>
+		<v-col cols=12 md=8>
+			<v-simple-table fixed-header>
+				<template v-slot:default>
+					<thead>
+						<tr>
+							<th class="text-left">Nome</th>
+							<th class="text-left">Sigla</th>
+							<th class="text-left">Votos</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="c in candidates" v-bind:key="c">
+							<td>{{ c.name }}</td>
+							<td>{{ c.party }}</td>
+							<td>{{ c.totalVotes }}</td>
+						</tr>
+					</tbody>
+				</template>
+			</v-simple-table>
+		</v-col>
+	</v-row>
 </template>
 
 <script>
   export default {
     name: 'HomeView',
-
-    components: {
-    },
+		data: () => ({
+			candidates: []
+		})
   }
 </script>
--- src/views/MyProfileView.vue
+++ src/views/MyProfileView.vue
@@ -1,13 +1,16 @@
 <template>
-	<h1>My Profile</h1>
+	<v-row>
+		<v-col cols=12 md=8>
+			<h1>My Profile</h1>
+		</v-col>
+		<v-col cols=12 md=8>
+			//
+		</v-col>
+	</v-row>
 </template>
 
 <script>
   export default {
     name: 'MyProfileView',
-
-    components: {
-    
-    },
   }
 </script>
