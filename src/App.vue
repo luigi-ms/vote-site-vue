1c1e92b28b9f1fabb8ec89dbbf4f7a25ba77051f
Luigi Moraes <luigimoraes3@gmail.com>
2022-05-31 22:22:30 -0300

    VoteView using v-form

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <v-app>
-		<v-app-bar dense app>
+		<v-app-bar 
+			dense 
+			color="deep-purple darken-4"
+			dark
+			app>
 			<v-toolbar-title>Voting Site</v-toolbar-title>
 
 			<template v-slot:extension>
 				<v-tabs>
 					<v-tab to="/">Homepage</v-tab>
 					<v-tab to="/vote">Vote</v-tab>
-					<v-tab to="/signIn">SignIn</v-tab>
+					<v-tab to="/profile">My Profile</v-tab>
 				</v-tabs>
 			</template>
 		</v-app-bar>
 
-		<v-main>
+		<v-main color="indigo lighten-5">
 			<v-container fluid>
 				<router-view/>
 			</v-container>
--- src/views/VoteView.vue
+++ src/views/VoteView.vue
@@ -1,12 +1,36 @@
 <template>
-	<h1>Vote Site</h1>
+	<v-row>
+		<v-col cols=12 md=8>
+			<h1>Vote Area</h1>
+		</v-col>
+		<v-col cols=12 md=8>
+			<v-form>
+				<v-container>
+					<v-row>
+						<v-col cols=10 md=6>
+							<v-text-field 
+								label="Voter number"
+								required/>
+						</v-col>
+						<v-col cols=10 md=6>
+							<v-text-field 
+								label="Candidate number"
+								required/>
+						</v-col>
+						<v-col cols=6 md=4>
+							<v-btn depressed color="primary">
+								Vote
+							</v-btn>
+						</v-col>
+					</v-row>
+				</v-container>
+			</v-form>
+		</v-col>
+	</v-row>
 </template>
 
 <script>
   export default {
     name: 'VoteView',
-
-    components: {
-    },
   }
 </script>
